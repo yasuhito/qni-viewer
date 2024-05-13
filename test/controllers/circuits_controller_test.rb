7da606fce708f76378dc28799ba1ba15bbbeddfb
Yasuhito Takamiya <yasuhito@hey.com>
2024-05-13 09:30:00 +0900

    X ゲートの JSON ドキュメントを追加 (#13)

--- test/controllers/circuits_controller_test.rb
+++ test/controllers/circuits_controller_test.rb
@@ -41,9 +41,9 @@ class CircuitsControllerTest
 
       assert_equal 4, amplitudes.length
       assert_equal '√½', amplitudes[0].to_h
-      assert_equal '0', amplitudes[1].to_h
+      assert_equal 0, amplitudes[1]
       assert_equal '√½', amplitudes[2].to_h
-      assert_equal '0', amplitudes[3].to_h
+      assert_equal 0, amplitudes[3]
     end
 
     test <<~TEST do
@@ -64,32 +64,48 @@ class CircuitsControllerTest
   end
 
   class XGateTest < GateTest
-    test 'X(0) 回路を計算' do
+    test <<~TEST do
+         ┌───┐
+      q: ┤ X ├
+         └───┘
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [["X"]] }' }, as: :json
 
       assert_equal 2, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '1', amplitudes[1].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 1, amplitudes[1]
     end
 
-    test 'X(1) 回路を計算' do
+    test <<~TEST do
+      q_0: ─────
+           ┌───┐
+      q_1: ┤ X ├
+           └───┘
+      end
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [[1, "X"]] }' }, as: :json
 
       assert_equal 4, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '0', amplitudes[1].to_h
-      assert_equal '1', amplitudes[2].to_h
-      assert_equal '0', amplitudes[3].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 0, amplitudes[1]
+      assert_equal 1, amplitudes[2]
+      assert_equal 0, amplitudes[3]
     end
 
-    test 'X(1, 2) 回路を計算' do
+    test <<~TEST do
+           ┌───┐
+      q_0: ┤ X ├
+           ├───┤
+      q_1: ┤ X ├
+           └───┘
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [["X", "X"]] }' }, as: :json
 
       assert_equal 4, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '0', amplitudes[1].to_h
-      assert_equal '0', amplitudes[2].to_h
-      assert_equal '1', amplitudes[3].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 0, amplitudes[1]
+      assert_equal 0, amplitudes[2]
+      assert_equal 1, amplitudes[3]
     end
   end
 
