ee478589a7aa07fd3f16da059048c5973c6d09a4
Yasuhito Takamiya <yasuhito@hey.com>
2024-05-13 09:47:17 +0900

    Y ゲートの JSON ドキュメントを追加 (#15)

--- test/controllers/circuits_controller_test.rb
+++ test/controllers/circuits_controller_test.rb
@@ -110,32 +110,48 @@ class CircuitsControllerTest
   end
 
   class YGateTest < GateTest
-    test 'Y(0) 回路を計算' do
+    test <<~TEST do
+         ┌───┐
+      q: ┤ Y ├
+         └───┘
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [["Y"]] }' }, as: :json
 
       assert_equal 2, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
+      assert_equal 0, amplitudes[0]
       assert_equal 'i', amplitudes[1].to_h
     end
 
-    test 'Y(1) 回路を計算' do
+    test <<~TEST do
+      q_0: ─────
+           ┌───┐
+      q_1: ┤ Y ├
+           └───┘
+      end
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [[1, "Y"]] }' }, as: :json
 
       assert_equal 4, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '0', amplitudes[1].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 0, amplitudes[1]
       assert_equal 'i', amplitudes[2].to_h
-      assert_equal '0', amplitudes[3].to_h
+      assert_equal 0, amplitudes[3]
     end
 
-    test 'Y(1, 2) 回路を計算' do
+    test <<~TEST do
+           ┌───┐
+      q_0: ┤ Y ├
+           ├───┤
+      q_1: ┤ Y ├
+           └───┘
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [["Y", "Y"]] }' }, as: :json
 
       assert_equal 4, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '0', amplitudes[1].to_h
-      assert_equal '0', amplitudes[2].to_h
-      assert_equal '-1', amplitudes[3].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 0, amplitudes[1]
+      assert_equal 0, amplitudes[2]
+      assert_equal(-1, amplitudes[3])
     end
   end
 
