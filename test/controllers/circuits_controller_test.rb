496923e1390388cc4ee24670b7de65110002cf8d
Yasuhito Takamiya <yasuhito@hey.com>
2024-05-13 10:10:58 +0900

    write1 ゲートの JSON ドキュメントを追加

--- test/controllers/circuits_controller_test.rb
+++ test/controllers/circuits_controller_test.rb
@@ -292,32 +292,48 @@ class CircuitsControllerTest
   end
 
   class Write1GateTest < GateTest
-    test '|1>(0) 回路を計算' do
+    test <<~TEST do
+         ┌───┐
+      q: ┤|1>├
+         └───┘
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [["|1>"]] }' }, as: :json
 
       assert_equal 2, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '1', amplitudes[1].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 1, amplitudes[1]
     end
 
-    test '|1>(1) 回路を計算' do
+    test <<~TEST do
+      q_0: ─────
+           ┌───┐
+      q_1: ┤|1>├
+           └───┘
+      end
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [[1, "|1>"]] }' }, as: :json
 
       assert_equal 4, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '0', amplitudes[1].to_h
-      assert_equal '1', amplitudes[2].to_h
-      assert_equal '0', amplitudes[3].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 0, amplitudes[1]
+      assert_equal 1, amplitudes[2]
+      assert_equal 0, amplitudes[3]
     end
 
-    test '|1>(1, 2) 回路を計算' do
+    test <<~TEST do
+           ┌───┐
+      q_0: ┤|1>├
+           ├───┤
+      q_1: ┤|1>├
+           └───┘
+    TEST
       get circuit_path, params: { circuit_json: '{ "cols": [["|1>", "|1>"]] }' }, as: :json
 
       assert_equal 4, amplitudes.length
-      assert_equal '0', amplitudes[0].to_h
-      assert_equal '0', amplitudes[1].to_h
-      assert_equal '0', amplitudes[2].to_h
-      assert_equal '1', amplitudes[3].to_h
+      assert_equal 0, amplitudes[0]
+      assert_equal 0, amplitudes[1]
+      assert_equal 0, amplitudes[2]
+      assert_equal 1, amplitudes[3]
     end
   end
 
