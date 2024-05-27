c199a98fd80dae02bd71d212f9c3dbadd3546857
Yasuhito Takamiya <yasuhito@hey.com>
2024-05-27 10:12:47 +0900

    リファクタリング

--- test/unit/unicode_fraction_test.rb
+++ test/unit/unicode_fraction_test.rb
@@ -5,8 +5,6 @@ require 'test_helper'
 require 'unicode_fraction'
 
 class UnicodeFractionTest < ActiveSupport::TestCase
-  # TODO: UnicodeFraction.from_number(1.0 / 2) を UnicodeFraction() に統一
-  #
   # rubocop:disable Metrics/ClassLength
   class FromStringTest < ActiveSupport::TestCase
     test '0' do
@@ -295,102 +293,102 @@ class UnicodeFractionTest < ActiveSupport::TestCase
 
   # TODO: Numeric との比較テストも追加 (assert_equal 0, UnicodeFraction.from_number(0))
   class FromNumberTest < ActiveSupport::TestCase
-    test 'from 0' do
+    test '0' do
       fraction = UnicodeFraction(0)
       assert_equal '0', fraction.to_s
     end
 
-    test 'from 1' do
+    test '1' do
       fraction = UnicodeFraction(1)
       assert_nil fraction
     end
 
-    test 'from 1/2' do
+    test '1/2' do
       fraction = UnicodeFraction(1.0 / 2)
       assert_equal '½', fraction.to_s
     end
 
-    test 'from 1/4' do
+    test '1/4' do
       fraction = UnicodeFraction(1.0 / 4)
       assert_equal '¼', fraction.to_s
     end
 
-    test 'from 3/4' do
+    test '3/4' do
       fraction = UnicodeFraction(3.0 / 4)
       assert_equal '¾', fraction.to_s
     end
 
-    test 'from 1/3' do
+    test '1/3' do
       fraction = UnicodeFraction(1.0 / 3)
       assert_equal '⅓', fraction.to_s
     end
 
-    test 'from 2/3' do
+    test '2/3' do
       fraction = UnicodeFraction(2.0 / 3)
       assert_equal '⅔', fraction.to_s
     end
 
-    test 'from 1/5' do
+    test '1/5' do
       fraction = UnicodeFraction(1.0 / 5)
       assert_equal '⅕', fraction.to_s
     end
 
-    test 'from 2/5' do
+    test '2/5' do
       fraction = UnicodeFraction(2.0 / 5)
       assert_equal '⅖', fraction.to_s
     end
 
-    test 'from 3/5' do
+    test '3/5' do
       fraction = UnicodeFraction(3.0 / 5)
       assert_equal '⅗', fraction.to_s
     end
 
-    test 'from 4/5' do
+    test '4/5' do
       fraction = UnicodeFraction(4.0 / 5)
       assert_equal '⅘', fraction.to_s
     end
 
-    test 'from 1/6' do
+    test '1/6' do
       fraction = UnicodeFraction(1.0 / 6)
       assert_equal '⅙', fraction.to_s
     end
 
-    test 'from 5/6' do
+    test '5/6' do
       fraction = UnicodeFraction(5.0 / 6)
       assert_equal '⅚', fraction.to_s
     end
 
-    test 'from 1/7' do
+    test '1/7' do
       fraction = UnicodeFraction(1.0 / 7)
       assert_equal '⅐', fraction.to_s
     end
 
-    test 'from 1/8' do
+    test '1/8' do
       fraction = UnicodeFraction(1.0 / 8)
       assert_equal '⅛', fraction.to_s
     end
 
-    test 'from 3/8' do
+    test '3/8' do
       fraction = UnicodeFraction(3.0 / 8)
       assert_equal '⅜', fraction.to_s
     end
 
-    test 'from 5/8' do
+    test '5/8' do
       fraction = UnicodeFraction(5.0 / 8)
       assert_equal '⅝', fraction.to_s
     end
 
-    test 'from 7/8' do
+    test '7/8' do
       fraction = UnicodeFraction(7.0 / 8)
       assert_equal '⅞', fraction.to_s
     end
 
-    test 'from 1/9' do
+    test '1/9' do
       fraction = UnicodeFraction(1.0 / 9)
       assert_equal '⅑', fraction.to_s
     end
 
-    test 'from 1/10' do
+    test '1/10' do
       fraction = UnicodeFraction(1.0 / 10)
       assert_equal '⅒', fraction.to_s
     end
