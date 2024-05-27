0e079ba27a89ec4856a2d80691af62de7f334a3c
Yasuhito Takamiya <yasuhito@hey.com>
2024-05-27 13:28:20 +0900

    UnicodeFraction#/ を追加

--- lib/unicode_fraction.rb
+++ lib/unicode_fraction.rb
@@ -91,6 +91,10 @@ class UnicodeFraction < Numeric
     @value - other
   end
 
+  def /(other)
+    @value / other
+  end
+
   def to_s
     @string
   end
---
+++ test/unit/unicode_fraction_div_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'unicode_fraction'
+
+class UnicodeFractionDivTest < ActiveSupport::TestCase
+  test '"1/2" / 2' do
+    assert_equal 0.25, UnicodeFraction('1/2') / 2
+  end
+
+  test '"½" / 2' do
+    assert_equal 0.25, UnicodeFraction('½') / 2
+  end
+
+  test '"√¼" / 2' do
+    assert_equal 0.25, UnicodeFraction('√¼') / 2
+  end
+
+  test '0.5 / 2' do
+    assert_equal 0.25, UnicodeFraction(0.5) / 2
+  end
+
+  test '√½ / 2' do
+    assert_equal Math.sqrt(1 / 2.0) / 2, UnicodeFraction(Math.sqrt(1 / 2.0)) / 2
+  end
+end
