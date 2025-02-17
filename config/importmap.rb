bfe68c2798fb60b3975460fac83b609238c8ec10
Yasuhito Takamiya <yasuhito@hey.com>
2025-02-17 09:49:08 +0900

    オフライン環境でも動作可能にする (#71)

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,7 @@ pin 'application'
 pin '@rails/actioncable', to: 'actioncable.esm.js'
 pin_all_from 'app/javascript/channels', under: 'channels'
 pin '@qni/common', to: '@qni--common.js' # @0.0.86
+pin '@qni/elements', to: '@qni--elements.js' # @0.0.87
 pin 'ansi-styles' # @5.2.0
 pin 'fraction.js' # @4.2.1
 pin 'pretty-format' # @29.7.0
